33b6f2371c228d770708cfa6ee0835ffd8230e87
RiyaBhandari-2811 <85748264+RiyaBhandari-2811@users.noreply.github.com>
2021-10-02 00:05:17 +0530

    Create StringInput.java
    Added the solution for issue #2 in Package Basics -> StringInput.java

---
+++ Basics/StringInput.java
@@ -0,0 +1,29 @@
+//  Contributor info : 
+//  Name: Riya Bhandari
+//   Github: https://github.com/RiyaBhandari-2811
+
+
+
+/**
+ *👉 Problem: String Input 
+ *Description: Write a Program to take string input from user and display it in java 
+ */
+
+package Basics;
+import java.util.Scanner;
+public class StringInput {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+
+        // Taking Input 
+        System.out.println(" Enter the String : ");
+        String s1 = sc.nextLine();
+
+
+        // Displaying It
+        System.out.print(" Your Entered String is : " + s1);
+
+
+    }
+}
